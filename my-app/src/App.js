efead5037e1268e5f85b34cbe63cb36f3efa967e
Лузина Мария <kaminskaya.mari@list.ru>
2024-07-21 20:12:53 +0700

    JSX App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,29 @@
+// декларативный
 import logo from './logo.svg';
 import './App.css';
-import { createElement } from 'react';
 
+// императивный
 export const App = () => {
 	const year = new Date().getFullYear();
-	return createElement(
-		'div',
-		{ className: 'App' },
-		createElement(
-			'header',
-			{ className: 'App-header' },
-			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-			createElement(
-				'p',
-				null,
-				'Edit',
-				createElement('code', null, 'src/App.js'),
-				'and save to reload.',
-			),
-			createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-			createElement('div', null, year),
-		),
+
+	return (
+		// декларативный
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<div>{year}</div>
+			</header>
+		</div>
 	);
 };
